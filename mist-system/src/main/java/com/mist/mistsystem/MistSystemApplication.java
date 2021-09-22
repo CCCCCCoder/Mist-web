303ef12c0abb040f53342459ad23f85884d6b5ab
民瑞 李 <Manley_Li@outlook.com>
2021-09-22 16:49:27 +0800

    init

---
+++ mist-system/src/main/java/com/mist/mistsystem/MistSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mist.mistsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MistSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MistSystemApplication.class, args);
+	}
+
+}
---
+++ mist-system/src/test/java/com/mist/mistsystem/MistSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mist.mistsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MistSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
